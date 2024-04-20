f53ed69e9709265dacf442df2977e4f8955c42be
Raj Poudel <raj.poudeel@gmail.com>
2024-04-20 21:44:19 +0545

    index js

--- day10/Server/index.js
+++ day10/Server/index.js
@@ -1,12 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const  mongoose = require('mongoose');
-const Usermodel  = require('./model/model');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
 const router = require('./routes/route');
-const secretKey = 'yourSecretKey'; 
 const PORT = 4000;
 mongoose.connect('mongodb://127.0.0.1:27017/day10after4thsem').then( (e)=> { console.log("mongodb connectd")});
 
@@ -14,6 +10,7 @@ const app = express();
 
 
 app.use(bodyParser.json());
+
 app.use(cors())
 app.use(express.json());
 app.get('/' , (req,res) =>{
