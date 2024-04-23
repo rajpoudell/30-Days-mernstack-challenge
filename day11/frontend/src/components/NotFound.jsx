620ca78a2814f635db786324e000ce16b502b0ab
Raj Poudel <raj.poudeel@gmail.com>
2024-04-23 10:10:05 +0545

    git day11 added

---
+++ day11/frontend/src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Notfound.css"
+export const Notfound= () => {
+    return (
+        <div className="not-found-container">
+          <h1 className='notfound'>404 - Not Found</h1>
+          <p>Sorry, the page you are looking for does not exist.</p>
+        </div>
+      );
+    };
