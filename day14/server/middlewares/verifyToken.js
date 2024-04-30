f80d70b53a7b42955916afa5b2ab41b292e154a9
Raj Poudel <raj.poudeel@gmail.com>
2024-04-30 23:29:03 +0545

    middleware for verify tokens

---
+++ day14/server/middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const secretKey = 'secret';  
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized - No token provided' });
+    }
+  
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized - Invalid token' });
+      }
+  
+      // Attach the decoded user information to the request for later use in the protected route
+      req.user = decoded;
+      return next();
+    });
+  };
+  module.exports = verifyToken;
