3d4c26b932f49ad79e19061b64ff19f67aaed253
Raj Poudel <raj.poudeel@gmail.com>
2024-05-11 18:28:05 +0545

    added all on clients side

--- day15/frontend/src/App.jsx
+++ day15/frontend/src/App.jsx
@@ -1,15 +1,29 @@
-// import { useState } from 'react'
-
+import { useTranslation } from 'react-i18next';
 import './App.css'
+import LangSelector from './components/LangSelector';
 
 function App() {
 
+  const { t } = useTranslation();
+
+  const {line1,line2} = t("description",{
+    channel:"Raj"
+  });
 
   return (
-    <>
-      This is app.
-    </>
+    <div className='container'>
+      <LangSelector/>
+      <h1>{t("greeting")}</h1>
+      <p>
+      {line1}
+      </p>
+      <p>
+      {line2}
+      </p>
+    </div>
   )
 }
 
 export default App
+
+// npm install react-i18next i18next i18next-browser-languagedetector --save~
