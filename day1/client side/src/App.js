ecb5dd3da02bca9f5a1f39193a5dd1c9e136c9d1
Raj Poudel <raj.poudeel@gmail.com>
2024-06-10 12:09:51 +0545

    local host

--- day1/client side/src/App.js
+++ day1/client side/src/App.js
@@ -6,7 +6,7 @@ function App() {
 const [backendData, setBackendData] = useState([{}]);
 
 useEffect(() => {
-  fetch("/api").then(
+  fetch("http://localhost:3000/api").then(
     response => response.json()
   ).then(
     data =>{
