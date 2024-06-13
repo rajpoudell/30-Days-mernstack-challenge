194d3bc1b8b47762850c2ee14003849819266cab
Raj Poudel <raj.poudeel@gmail.com>
2024-06-13 13:42:16 +0545

    result added

--- day4/index.js
+++ day4/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use('/',router);
 
