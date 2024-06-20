d1c6c76132c985a2257e1f6047d0113a2f3b4176
Raj Poudel <raj.poudeel@gmail.com>
2024-06-20 11:02:20 +0545

    databses setting improve

--- day10/Server/index.js
+++ day10/Server/index.js
@@ -1,16 +1,17 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const  mongoose = require('mongoose');
 const cors = require('cors');
 const router = require('./routes/route');
 const PORT = 4000;
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017//day10after4thsem';
-mongoose.connect(mongoURI).then( (e)=> { console.log("mongodb connectd")});
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/day10after4thsem';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 const app = express();
 
 // mongodb://127.0.0.1:27017
-app.use(bodyParser.json());
 
 app.use(cors())
 app.use(express.json());
